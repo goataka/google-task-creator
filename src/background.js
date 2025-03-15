3417673925b6edd58cb1cd3f13daf8813ccdb772
GOAMI_Takaaki <tkgoami@gmail.com>
2025-03-15 14:20:28 +0900

    #36 suite manifest v3 and element problem

--- src/background.js
+++ src/background.js
@@ -1,4 +1,4 @@
-const GOOGLE_CALENDAR_WEEK_URL = 'https://calendar.google.com/calendar/u/0/r/week';
+const GOOGLE_TASKS_URL = 'https://tasks.google.com/tasks/';
 
 function sendMessage(tab, title, explanation, retryCount) {
     if (retryCount <= 0) return;
@@ -12,7 +12,9 @@ function sendMessage(tab, title, explanation, retryCount) {
                 title: title,
                 explanation: explanation
             };
-            chrome.tabs.sendMessage(tab.id, options, function (response) {});
+            chrome.tabs.sendMessage(tab.id, options, function (response) {
+                console.log(response);
+            });
         } else {
             setTimeout( () => {
                 sendMessage(tab, title, explanation, retryCount-1);
@@ -23,7 +25,7 @@ function sendMessage(tab, title, explanation, retryCount) {
 
 const SEND_RETRY = 1000;
 function createTask(title, explanation) {
-    chrome.tabs.create({ url: GOOGLE_CALENDAR_WEEK_URL }, tab => {
+    chrome.tabs.create({ url: GOOGLE_TASKS_URL }, tab => {
         sendMessage(tab, title, explanation, SEND_RETRY)
     });
 }
@@ -34,22 +36,23 @@ chrome.action.onClicked.addListener(tab => {
 
 chrome.runtime.onInstalled.addListener(() => {
     const parent = chrome.contextMenus.create({
-        id: "create_task_menu",
+        id: "create_task",
         title: "Create Task",
         type: "normal",
         contexts: ["link"]
     });
 });
-  
-chrome.contextMenus.onClicked.addListener((item) => {
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-        if (!tabs[0]) return;
-        const currentTab = tabs[0];
-        const options = {
-            action: "getLink"
-        };
-        chrome.tabs.sendMessage(currentTab.id, options, function(response) {
+
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+    switch (info.menuItemId) {
+        case "create_task":
+            const options = {
+                action: "getLink",
+            };
+          chrome.tabs.sendMessage(tab.id, options, function (response) {
+            console.log(response);
             createTask(response.title, response.url);
-        });
-    });
+          });
+          break;
+    }
 });
